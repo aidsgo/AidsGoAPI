61644b63df74e109d60007d7414a2e9082f4f325
Yan Yu <yu.yan@rea-group.com>
2016-10-28 22:34:42 +0800

    add elders table

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161028143407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "elders", force: :cascade do |t|
+    t.string   "name"
+    t.date     "birthday"
+    t.string   "sex"
+    t.string   "community"
+    t.string   "image"
+    t.jsonb    "contact"
+    t.integer  "help_count"
+    t.jsonb    "emergency_call"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
 end
