297a7d41560a6453c2b1437d0947b82848950f15
Yan Yu <yu.yan@rea-group.com>
2016-11-26 10:50:12 +0800

    move volunteeer actions to volunteer controller

--- app/controllers/volunteer_controller.rb
+++ app/controllers/volunteer_controller.rb
@@ -1,5 +1,62 @@
+require_relative '../connectors/wilddog_connector'
+
 class VolunteerController < ApplicationController
   def show_all
     render json: Volunteer.all
+  end
+
+  def accept
+    volunteer_id = params[:volunteer_id]
+    emergency = Emergency.find(params[:emergency_id])
+    WildDogConnector.new.add_volunteer_to_incident emergency.id, volunteer_id
+    accepted_volunteers = emergency.accept
+    if accepted_volunteers.nil?
+      accepted_volunteers = [volunteer_id]
+    else
+      accepted_volunteers = accepted_volunteers << volunteer_id unless accepted_volunteers.include?(volunteer_id)
+    end
+    emergency.update_attributes(accept: accepted_volunteers)
+
+    render nothing: true, status: :ok
+  end
+
+
+  def my_taken_incidents
+    my_incidents = Emergency.all.select do |emergency|
+      emergency.accept.include?(params[:volunteer_id])
+    end
+
+    results ={}
+    my_incidents.each do |incident|
+      emergency_elder = Elder.find(incident.elder_id)
+      results.merge!(
+        {
+          incident.id => {
+            id: incident.id,
+            name: Elder.find(incident.elder_id).name,
+            location: incident.elder_location,
+            time: incident.created_at,
+            taken: incident.accept,
+            resolved: incident.resolved,
+            emergency_call: emergency_elder.emergency_call['phone'],
+            property_management_company_phone: emergency_elder.emergency_call['pmc_phone']
+          }
+        }
+      )
+    end
+
+    render json: results
+  end
+
+  def update_resolved
+    emergency = Emergency.find(params[:emergency_id])
+    if emergency.resolved.to_s.empty? && emergency.update(resolved: params[:volunteer_id])
+      @wd_connector.resolve_incident params[:emergency_id]
+      # notify_folks(Elder.find(emergency.elder_id), 'Emergency has been resolved by volunteers!')
+
+      render json: emergency, status: :created
+    else
+      render error: 'Aid resolved fails', status: :unprocessable_entity
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,70 +1,13 @@
 Rails.application.routes.draw do
-
   post '/emergency', :to => 'emergency#notify'
-
-  put '/emergencies/:emergency_id/resolve/:volunteer_id', :to => 'emergency#update_resolved'
   put '/emergency/:action_name', :to => 'emergency#update_action'
-  put '/emergencies/:emergency_id/add/:volunteer_id', :to => 'emergency#accept'
-
   get '/emergencies', :to => 'emergency#show_emergency_list'
-  get '/emergencies/volunteers/:volunteer_id', :to => 'emergency#my_taken_incidents'
+
+  put '/volunteers/:volunteer_id/resolve/:emergency_id', :to => 'volunteer#update_resolved'
+  put '/volunteers/:volunteer_id/accept/:emergency_id', :to => 'volunteer#accept'
+  get '/volunteers/:volunteer_id/emergencies', :to => 'volunteer#my_taken_incidents'
   get '/volunteers', :to => 'volunteer#show_all'
 
   put '/login', :to => 'volunteer_login#login'
   post '/sign_up', :to => 'volunteer_login#sign_up'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
